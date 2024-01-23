094a0cf831b43cb338b69f0527211c1e25e2fc22
ollprogram <39884051+ollprogram@users.noreply.github.com>
2024-01-23 23:36:14 +0100

    started making configuration

---
+++ src/main/java/fr/ollprogram/twitchdiscordbridge/configuration/BridgeConfig.java
@@ -0,0 +1,55 @@
+/* Copyright © 2024 ollprogram
+ *
+ * This file is part of TwitchDiscordBridge.
+ * TwitchDiscordBridge is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or \(at your option\) any later version.
+ * TwitchDiscordBridge is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with TwitchDiscordBridge.
+ * If not, see https://www.gnu.org/licenses.
+ */
+package fr.ollprogram.twitchdiscordbridge.configuration;
+
+import org.jetbrains.annotations.NotNull;
+
+public interface BridgeConfig {
+
+    /**
+     * The twitch channel name
+     * @return The twitch channel name
+     */
+    @NotNull String getTwitchChannelName();
+
+    /**
+     * The discord channel ID
+     * @return The discord channel ID
+     */
+    @NotNull String getDiscordChannelID();
+
+    /**
+     * Get the twitch bot token
+     * @return The twitch bot token
+     */
+    @NotNull String getTwitchToken();
+
+    /**
+     * Get the discord bot token
+     * @return The discord bot token
+     */
+    @NotNull String getDiscordToken();
+
+    /**
+     * Change the discord channel ID in this configuration
+     * @param id The new discord channel ID
+     */
+    void changeDiscordChannelID(@NotNull String id);
+
+    /**
+     * Change the twitch channel name
+     * @param name The new twitch channel name
+     */
+    void changeTwitchChannelName(@NotNull String name);
+
+}
+
