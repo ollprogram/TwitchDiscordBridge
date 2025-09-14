88a47dd0b80dc0d7771e236a26698aad7f94c9f2
ollprogram <39884051+ollprogram@users.noreply.github.com>
2025-09-14 22:41:52 +0200

    update copyrights

--- src/test/java/fr/ollprogram/twitchdiscordbridge/configuration/save/ConfigSaverTest.java
+++ src/test/java/fr/ollprogram/twitchdiscordbridge/configuration/save/ConfigSaverTest.java
@@ -1,5 +1,5 @@
-/* Copyright © 2024 ollprogram
- *
+/*
+ * Copyright © 2025 ollprogram
  * This file is part of TwitchDiscordBridge.
  * TwitchDiscordBridge is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation, either version 3 of the License, or \(at your option\) any later version.
